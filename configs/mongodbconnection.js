8d6ff3d31f226bb30f87d89b37a2fa737eda4ed3
hari <haribabuji2004@gmail.com>
2025-06-24 12:52:00 +0530

    tested using postman and made some changes

--- configs/mongodbconnection.js
+++ configs/mongodbconnection.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const connectdb = async () => {
     try{
-        const conn=await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/urlshortener', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+        const conn=await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/urlshortener', {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true
         });
         console.log(`MongoDB connected: ${conn.connection.host}`);
     }catch(error){
         console.error(`Error connecting to MongoDB: ${error.message}`);
         process.exit(1);
     }
-}
+};
 module.exports = connectdb;
